5ec3eb57340f6a64272c4208838f57b8dd318ad7
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-15 11:41:47 -0300

    Initial commit

---
+++ src/main/java/org/example/TesteManeiro.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteManeiro {
+
+    @Test
+    public void TesteWeb() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https:\\www.google.com");
+
+    }
+
+}
